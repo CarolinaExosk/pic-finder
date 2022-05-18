1c468c117201831ff4be88fe87c1fd1c5b6d44c9
CarolinaExosk <01000101ac@gmail.com>
2022-05-18 09:00:00 -0300

    Fix: removendo return duplicado e organizando layout do App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import PerfilGaleria from "./components/PerfilGaleria";
 import Galeria from './components/Galeria';
 
 function App() {
-  return <PerfilGaleria />;
   return (
-      <div className="App">
+    <div className="App">
+      <PerfilGaleria />
+      <div style={{ marginTop: '20px' }}>
         <h1>Minha Galeria</h1>
         <Galeria />
       </div>
+    </div>
   );
 }
 
